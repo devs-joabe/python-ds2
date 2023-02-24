905872fe822b083120e3fd3c745c9bd27a57d8d0
Joabe Nascimento <63386616+devs-joabe@users.noreply.github.com>
2023-02-24 19:53:12 -0300

    Update aluno.py

--- Cadrastro de aluno/model/aluno.py
+++ Cadrastro de aluno/model/aluno.py
@@ -1,3 +1,7 @@
+# Essa classe também é conhecida como Business Object Model (objeto modelo de negócio). 
+# Sua responsabilidade é gerenciar e controlar a forma como os dados se comportam por meio das funções, 
+# lógica e regras de negócios estabelecidas.
+
 class Aluno:
     def __init__(self, nome, matricula, curso):
         self.nome = nome
