2f62787cdda6bc1f9974a0826c9706a5c89fd374
Joabe Nascimento <63386616+devs-joabe@users.noreply.github.com>
2023-02-24 19:52:55 -0300

    Update aluno_controller.py

--- Cadrastro de aluno/controller/aluno_controller.py
+++ Cadrastro de aluno/controller/aluno_controller.py
@@ -1,3 +1,8 @@
+#  Camada de controle é responsável por intermediar as requisições enviadas pelo View com as respostas fornecidas pelo Model, 
+# processando os dados que o usuário informou e repassando para outras camadas. 
+# Numa analogia bem simplista, o controller  operaria como o ‘’maestro de uma orquestra’’  
+# que permite a comunicação entre o detentor dos dados e a pessoa com vários questionamentos no MVC
+
 from aluno_view import *
 from aluno import *
 
