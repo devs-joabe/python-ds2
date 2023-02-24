fae43202651aa435826ff9906041609ca94146b0
Joabe Nascimento <63386616+devs-joabe@users.noreply.github.com>
2023-02-24 19:52:05 -0300

    MVC

---
+++ Cadrastro de aluno/controller/aluno_controller.py
@@ -0,0 +1,8 @@
+from aluno_view import *
+from aluno import *
+
+root = Tk()
+
+view = AlunoView(root)
+
+root.mainloop()
---
+++ Cadrastro de aluno/view/aluno_view.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from tkinter import ttk
+
+from aluno import Aluno
+
+class AlunoView:
+    def __init__(self, master):
+        self.frame = Frame(master)
+        self.frame.pack()
+
+        self.nome_label = Label(self.frame, text="Nome:")
+        self.nome_label.grid(row=0, column=0)
+
+        self.nome_entry = Entry(self.frame)
+        self.nome_entry.grid(row=0, column=1)
+
+        self.matricula_label = Label(self.frame, text="Matrícula:")
+        self.matricula_label.grid(row=1, column=0)
+
+        self.matricula_entry = Entry(self.frame)
+        self.matricula_entry.grid(row=1, column=1)
+
+        self.curso_label = Label(self.frame, text="Curso:")
+        self.curso_label.grid(row=2, column=0)
+
+        self.curso_entry = Entry(self.frame)
+        self.curso_entry.grid(row=2, column=1)
+
+        self.cadastrar_button = Button(self.frame, text="Cadastrar", command=self.cadastrar)
+        self.cadastrar_button.grid(row=3, column=0)
+
+        self.alunos_table = ttk.Treeview(self.frame, columns=("Nome", "Matrícula", "Curso"))
+        self.alunos_table.heading("Nome", text="Nome")
+        self.alunos_table.heading("Matrícula", text="Matrícula")
+        self.alunos_table.heading("Curso", text="Curso")
+        self.alunos_table.grid(row=4, columnspan=2)
+
+    def cadastrar(self):
+        nome = self.nome_entry.get()
+        matricula = self.matricula_entry.get()
+        curso = self.curso_entry.get()
+
+        aluno = Aluno(nome, matricula, curso)
+        self.alunos_table.insert("", "end", text=aluno.nome, values=(aluno.nome, aluno.matricula, aluno.curso))
